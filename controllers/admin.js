9e50fa4dce10f6baf6096cb47b70ba8eb6ee76c7
juanbernalGL <juan.bernal@gorillalogic.com>
2021-04-15 16:15:14 -0500

    Initial commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,13 +17,30 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  console.log(image);
+  if( image ) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: 'Attached file is not an image',
+      validationErrors: []
+    });
+  }
   const errors = validationResult(req);
 
+
+
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
@@ -38,6 +55,7 @@ exports.postAddProduct = (req, res, next) => {
       errorMessage: errors.array()[0].msg,
       validationErrors: errors.array()
     });
+    console.log(errors.array());
   }
 
   const product = new Product({
